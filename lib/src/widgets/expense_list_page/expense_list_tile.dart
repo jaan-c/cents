d876ae6ee3d645e57858861add4488004533df2f
jaan-c <jaan-c@users.noreply.github.com>
2021-03-29 11:44:59 +0800

    Show creation time on expense list tile.

--- lib/src/widgets/expense_list_page/expense_list_tile.dart
+++ lib/src/widgets/expense_list_page/expense_list_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cents/src/domain/expense.dart';
 import 'package:flutter/material.dart';
+import '../util/datetime.dart';
 
 class ExpenseListTile extends StatelessWidget {
   final Expense expense;
@@ -8,11 +9,34 @@ class ExpenseListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: _categoryText(context),
-      trailing: _costText(context),
-      subtitle: expense.note.isNotEmpty ? _noteText(context) : null,
+    return Padding(
+      child: Column(
+        children: [
+          _createdAtText(context),
+          Row(
+            children: [
+              Expanded(child: _categoryText(context)),
+              _costText(context),
+            ],
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+          ),
+          _noteText(context),
+        ],
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+      padding: EdgeInsets.all(16),
     );
+  }
+
+  Widget _createdAtText(BuildContext context) {
+    final content = expense.createdAt.relativeDisplay();
+
+    final textTheme = Theme.of(context).textTheme;
+
+    return Text(content, style: textTheme.overline);
   }
 
   Widget _categoryText(BuildContext context) {
@@ -22,17 +46,22 @@ class ExpenseListTile extends StatelessWidget {
   }
 
   Widget _costText(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
     final content =
         '\u20B1${expense.cost.units}.${expense.cost.cents.toString().padLeft(2, '0')}';
+
+    final textTheme = Theme.of(context).textTheme;
 
     return Text(content, style: textTheme.subtitle1);
   }
 
   Widget _noteText(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
 
-    return Text(expense.note, style: textTheme.subtitle2);
+    return Text(
+      expense.note,
+      style: textTheme.subtitle2
+          ?.apply(color: theme.colorScheme.onSurface.withOpacity(0.6)),
+    );
   }
 }
