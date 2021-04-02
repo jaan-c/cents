95deb35656091daa60606e529657d1ffbf22509e
jaan-c <jaan-c@users.noreply.github.com>
2021-04-02 19:55:22 +0800

    Be explicit about what zero id means.

--- app/src/main/java/jaanc/cents/domain/Expense.kt
+++ app/src/main/java/jaanc/cents/domain/Expense.kt
@@ -3,9 +3,13 @@ package jaanc.cents.domain
 import java.time.LocalDateTime
 
 data class Expense(
-    val id: Int = 0,
+    val id: Int = UNSET_ID,
     val cost: Amount = Amount.zero(),
     val category: ExpenseCategory = ExpenseCategory.uncategorized(),
     val note: String = "",
     val createdAt: LocalDateTime = LocalDateTime.now(),
-)
+) {
+    companion object {
+        const val UNSET_ID = 0
+    }
+}
