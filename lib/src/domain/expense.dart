f8fd724448c61d7d4edeeeb357f628b9cba56a5f
jaan-c <jaan-c@users.noreply.github.com>
2021-04-17 03:24:03 +0800

    Reorder properties.

--- lib/src/domain/expense.dart
+++ lib/src/domain/expense.dart
@@ -7,28 +7,28 @@ class Expense {
   static const UNSET_ID = 0;
 
   final int id;
-  final Amount cost;
   final ExpenseCategory category;
+  final Amount cost;
   final DateTime createdAt;
   final String note;
 
   Expense(
       {this.id = UNSET_ID,
-      required this.cost,
       required this.category,
+      required this.cost,
       required this.createdAt,
       this.note = ''});
 
   Expense copyWith(
       {int? id = 0,
-      Amount? cost,
       ExpenseCategory? category,
+      Amount? cost,
       DateTime? createdAt,
       String? note}) {
     return Expense(
         id: id ?? this.id,
-        cost: cost ?? this.cost,
         category: category ?? this.category,
+        cost: cost ?? this.cost,
         createdAt: createdAt ?? this.createdAt,
         note: note ?? this.note);
   }
@@ -39,5 +39,5 @@ class Expense {
   }
 
   @override
-  int get hashCode => hashObjects([id, cost, category, createdAt, note]);
+  int get hashCode => hashObjects([id, category, cost, createdAt, note]);
 }
