5cece59adc6daf7faad221fae37223762e8f5c87
jaan-c <jaan-c@users.noreply.github.com>
2021-04-25 17:22:32 +0800

    Stats page initial implementation.

--- lib/src/widgets/expense_stats_page/expense_stats_page.dart
+++ lib/src/widgets/expense_stats_page/expense_stats_page.dart
@@ -1,4 +1,10 @@
+import 'package:cents/src/database/expense_provider.dart';
+import 'package:cents/src/domain/expense.dart';
+import 'package:cents/src/domain/summary.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'expense_stats_page_scaffold.dart';
 
 class ExpenseStatsPage extends StatefulWidget {
   @override
@@ -6,8 +12,64 @@ class ExpenseStatsPage extends StatefulWidget {
 }
 
 class _ExpenseStatsPageState extends State<ExpenseStatsPage> {
+  late ExpenseProvider provider;
+  var selectedYear = DateTime.now().year;
+  var allExpenses = <Expense>[];
+
+  Summary get summary => Summary(allExpenses);
+
+  bool get hasPreviousYear =>
+      summary.yearSummaries.containsKey(selectedYear - 1);
+  bool get hasNextYear => summary.yearSummaries.containsKey(selectedYear + 1);
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    setState(() {
+      provider = context.read<ExpenseProvider>();
+      _onProviderMutation();
+      provider.addListener(_onProviderMutation);
+    });
+  }
+
+  @override
+  void dispose() {
+    final provider = context.read<ExpenseProvider>();
+    provider.removeListener(_onProviderMutation);
+
+    super.dispose();
+  }
+
+  Future<void> _onProviderMutation() async {
+    final newAllExpenses = await provider.getAllExpenses();
+    setState(() {
+      allExpenses = newAllExpenses;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ExpenseStatsPageScaffold(
+      yearSummary: summary.yearSummaries[selectedYear],
+      onPreviousYear: hasPreviousYear ? _selectPreviousYear : null,
+      onNextYear: hasNextYear ? _selectNextYear : null,
+    );
+  }
+
+  void _selectPreviousYear() {
+    assert(hasPreviousYear);
+
+    setState(() {
+      selectedYear--;
+    });
+  }
+
+  void _selectNextYear() {
+    assert(hasNextYear);
+
+    setState(() {
+      selectedYear++;
+    });
   }
 }
--- lib/src/widgets/expense_stats_page/expense_stats_page_scaffold.dart
+++ lib/src/widgets/expense_stats_page/expense_stats_page_scaffold.dart
@@ -1,8 +1,55 @@
+import 'package:cents/src/domain/summary.dart';
+import 'package:cents/src/widgets/shared/month_summary_card.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseStatsPageScaffold extends StatelessWidget {
+  final YearSummary? yearSummary;
+  final VoidCallback? onPreviousYear;
+  final VoidCallback? onNextYear;
+
+  ExpenseStatsPageScaffold(
+      {this.yearSummary, this.onPreviousYear, this.onNextYear});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: _appBar(),
+      body: yearSummary != null ? _monthSummaryCards() : null,
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(
+        yearSummary != null ? '${yearSummary!.year} Summary' : 'No Expenses',
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.navigate_before_rounded),
+          onPressed: onPreviousYear,
+        ),
+        IconButton(
+          icon: Icon(Icons.navigate_next_rounded),
+          onPressed: onNextYear,
+        ),
+      ],
+    );
+  }
+
+  Widget _monthSummaryCards() {
+    final months = yearSummary!.monthSummaries.keys.toList()..sort();
+
+    return ListView.builder(
+      itemCount: months.length,
+      itemBuilder: (context, ix) {
+        final month = months[ix];
+        final monthSummary = yearSummary!.monthSummaries[month]!;
+
+        return MonthSummaryCard(
+          margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          monthSummary: monthSummary,
+        );
+      },
+    );
   }
 }
