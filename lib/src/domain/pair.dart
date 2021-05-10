2fb8639c1a5bc4a504384aa8239cd69f94611985
jaan-c <jaan-c@users.noreply.github.com>
2021-05-10 21:42:46 +0800

    Added Pair data structure.

---
+++ lib/src/domain/pair.dart
@@ -0,0 +1,18 @@
+import 'package:quiver/core.dart';
+
+class Pair<F, S> {
+  final F first;
+  final S second;
+
+  Pair(this.first, this.second);
+
+  @override
+  bool operator ==(dynamic other) {
+    return other is Pair<F, S> &&
+        first.hashCode == other.first.hashCode &&
+        second.hashCode == other.second.hashCode;
+  }
+
+  @override
+  int get hashCode => hash2(first, second);
+}
