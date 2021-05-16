28bc521ee3f861fd8eddd9d6d2f1a865b665e0dd
jaan-c <jaan-c@users.noreply.github.com>
2021-05-17 02:49:35 +0800

    Move category cost footer into its own file.

---
+++ lib/src/ui/widgets/month_summary_card/category_cost_grid.dart
@@ -0,0 +1,69 @@
+import 'package:cents/src/domain/amount.dart';
+import 'package:cents/src/domain/expense_category.dart';
+import 'package:flutter/material.dart';
+
+import 'fixed_grid.dart';
+import 'month_summary_card.dart';
+
+class CategoryCostGrid extends StatelessWidget {
+  final Map<ExpenseCategory, Amount> categoryCosts;
+  final TextToColor textToColor;
+
+  CategoryCostGrid({
+    required this.categoryCosts,
+    required this.textToColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = categoryCosts.keys.toList()..sort();
+
+    return FixedGrid(
+      crossAxisCount: 3,
+      children: [
+        for (final category in categories)
+          _categoryCostTile(
+            context: context,
+            category: category,
+            cost: categoryCosts[category]!,
+            textToColor: textToColor,
+          ),
+      ],
+    );
+  }
+
+  Widget _categoryCostTile({
+    required BuildContext context,
+    required ExpenseCategory category,
+    required Amount cost,
+    required TextToColor textToColor,
+  }) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          Icons.circle,
+          color: textToColor(theme.brightness, category.name),
+          size: 12,
+        ),
+        SizedBox(width: 8),
+        DefaultTextStyle(
+          style: textTheme.bodyText2!,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(category.name),
+              Text(cost.toLocalString()),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/src/ui/widgets/month_summary_card/month_summary_card_content.dart
+++ lib/src/ui/widgets/month_summary_card/month_summary_card_content.dart
@@ -1,10 +1,9 @@
 import 'package:cents/src/domain/amount.dart';
-import 'package:cents/src/domain/expense_category.dart';
 import 'package:cents/src/domain/summary.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import 'fixed_grid.dart';
+import 'category_cost_grid.dart';
 import 'month_summary_chart.dart';
 import 'month_summary_card.dart';
 
@@ -36,7 +35,6 @@ class MonthSummaryCardContent extends StatelessWidget {
         ),
         SizedBox(height: 16),
         _footer(
-          context: context,
           monthSummary: monthSummary,
           textToColor: textToColor,
         ),
@@ -67,7 +65,6 @@ class MonthSummaryCardContent extends StatelessWidget {
   }
 
   Widget _footer({
-    required BuildContext context,
     required MonthSummary monthSummary,
     required TextToColor textToColor,
   }) {
@@ -75,52 +72,9 @@ class MonthSummaryCardContent extends StatelessWidget {
     final categoryCosts = Map.fromEntries(categories
         .map((c) => MapEntry(c, monthSummary.totalCostBy(category: c))));
 
-    return FixedGrid(
-      crossAxisCount: 3,
-      children: [
-        for (final category in categories)
-          _footerTile(
-            context: context,
-            category: category,
-            cost: categoryCosts[category]!,
-            textToColor: textToColor,
-          )
-      ],
-    );
-  }
-
-  Widget _footerTile({
-    required BuildContext context,
-    required ExpenseCategory category,
-    required Amount cost,
-    required TextToColor textToColor,
-  }) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(
-          Icons.circle,
-          color: textToColor(theme.brightness, category.name),
-          size: 12,
-        ),
-        SizedBox(width: 8),
-        DefaultTextStyle(
-          style: textTheme.bodyText2!,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(category.name),
-              Text(cost.toLocalString()),
-            ],
-          ),
-        ),
-      ],
+    return CategoryCostGrid(
+      categoryCosts: categoryCosts,
+      textToColor: textToColor,
     );
   }
 }
