1eed25454fa65c935c15fa147627c5edf6dc423b
jaan-c <jaan-c@users.noreply.github.com>
2021-05-16 10:58:14 +0800

    Use own partitioned bar chart implementation instead of FLChart's convoluted widget.

--- lib/src/ui/widgets/month_summary_bar_chart.dart
+++ lib/src/ui/widgets/month_summary_bar_chart.dart
@@ -1,12 +1,12 @@
 import 'dart:math' as math;
 import 'package:cents/src/domain/amount.dart';
 import 'package:cents/src/domain/summary.dart';
-import 'package:cents/src/domain/week_of_month.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:quiver/iterables.dart';
 
 import 'month_summary_card.dart';
+import 'partitioned_bar_chart/partitioned_bar_chart.dart';
+import 'partitioned_bar_chart/partitioned_bar_data.dart';
 
 class MonthSummaryBarChart extends StatelessWidget {
   final MonthSummary monthSummary;
@@ -16,192 +16,75 @@ class MonthSummaryBarChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BarChart(_monthToChartData(context, monthSummary, textToColor));
+    final weeks = monthSummary.getAllWeeks();
+    final weekCosts = weeks
+        .map((w) => monthSummary.totalCostBy(weekOfMonth: w).toDouble())
+        .toList();
+    final maxWeekCost = max(weekCosts)!;
+    final ceilingCost = _ceilingByPlaceValue(maxWeekCost);
+
+    return PartitionedBarChart(
+      maxValue: ceilingCost,
+      barDatas: _monthSummaryToBarData(
+        context: context,
+        monthSummary: monthSummary,
+        textToColor: textToColor,
+      ),
+      magnitudePartitionCount: 4,
+      magnitudeToLabel: _magnitudeToLabel,
+    );
   }
 
-  BarChartData _monthToChartData(
-    BuildContext context,
-    MonthSummary monthSummary,
-    TextToColor textToColor,
-  ) {
-    if (monthSummary.isEmpty()) {
-      return BarChartData();
-    }
+  List<PartitionedBarData> _monthSummaryToBarData({
+    required BuildContext context,
+    required MonthSummary monthSummary,
+    required TextToColor textToColor,
+  }) {
+    final brightness = Theme.of(context).brightness;
 
     final weeks = monthSummary.getAllWeeks();
-    final weekCostsAsDouble =
-        weeks.map((w) => monthSummary.totalCostBy(weekOfMonth: w).toDouble());
-    final maxWeekCostAsDouble = max(weekCostsAsDouble)!;
-    final ceilingCostAsDouble =
-        math.max(10.0, _ceilingByPlaceValue(maxWeekCostAsDouble));
-    final costInterval = ceilingCostAsDouble / 3;
+    final weekCosts = weeks
+        .map((w) => monthSummary.totalCostBy(weekOfMonth: w).toDouble())
+        .toList();
+    final categories = monthSummary.getAllCategories();
 
-    return BarChartData(
-      // For some reason the top most horizontal line won't render sometimes if
-      // verticalInterval * 3 == maxY so make sure maxY will always be bigger.
-      maxY: ceilingCostAsDouble + 1,
-      groupsSpace: 8,
-      alignment: BarChartAlignment.spaceEvenly,
-      barGroups: [
-        for (final weekOfMonth in weeks)
-          _weekToBarData(context, monthSummary, weekOfMonth, textToColor)
-      ],
-      gridData: _gridData(
-        context: context,
-        horizontalLineInterval: costInterval,
-      ),
-      titlesData: _titlesData(
-        context: context,
-        verticalTitleInterval: costInterval,
-      ),
-      borderData: _borderData(context: context),
-      barTouchData: _barTouchData(context: context),
-    );
-  }
+    final barDatas = <PartitionedBarData>[];
 
-  BarChartGroupData _weekToBarData(
-    BuildContext context,
-    MonthSummary monthSummary,
-    WeekOfMonth weekOfMonth,
-    TextToColor textToColor,
-  ) {
-    final weekTotalCost = monthSummary.totalCostBy(weekOfMonth: weekOfMonth);
+    for (var i = 0; i < weeks.length; i++) {
+      final week = weeks[i];
+      final value = weekCosts[i];
 
-    return BarChartGroupData(
-      x: weekOfMonth.toInt(),
-      barRods: [
-        BarChartRodData(
-          y: weekTotalCost.toDouble(),
-          width: 32,
-          borderRadius: BorderRadius.circular(2),
-          rodStackItems: _weekToBarStackData(
-              context, monthSummary, weekOfMonth, textToColor),
-        ),
-      ],
-    );
-  }
+      final label = week.toOrdinalString();
+      final categoryCosts = categories.map((c) =>
+          monthSummary.totalCostBy(weekOfMonth: week, category: c).toDouble());
 
-  List<BarChartRodStackItem> _weekToBarStackData(
-    BuildContext context,
-    MonthSummary monthSummary,
-    WeekOfMonth weekOfMonth,
-    TextToColor textToColor,
-  ) {
-    var acc = Amount();
-    final stackData = <BarChartRodStackItem>[];
+      late final List<double> partitionFactors;
+      late final List<Color> partitionColors;
+      if (value == 0) {
+        partitionFactors = [1.0];
+        partitionColors = [Colors.blue];
+      } else {
+        partitionFactors = categoryCosts.map((c) => c / value).toList();
+        partitionColors =
+            categories.map((c) => textToColor(brightness, c.name)).toList();
+      }
 
-    // Reverse categories so ascending alphabetical arrangment of categories
-    // is preserved since stack is built from bottom up.
-    for (final category in monthSummary.getAllCategories().reversed) {
-      final weekCategoryCost = monthSummary.totalCostBy(
-          weekOfMonth: weekOfMonth, category: category);
-      final start = acc;
-      final end = start.add(weekCategoryCost);
-
-      final stack = BarChartRodStackItem(
-        start.toDouble(),
-        end.toDouble(),
-        textToColor(Theme.of(context).brightness, category.name),
+      final data = PartitionedBarData(
+        value: value,
+        label: label,
+        partitionFactors: partitionFactors,
+        partitionColors: partitionColors,
+        tooltipText: '$label Week\n${Amount.fromDouble(value).toLocalString()}',
       );
 
-      acc = end;
-      stackData.add(stack);
+      barDatas.add(data);
     }
 
-    return stackData;
+    return barDatas;
   }
 
-  FlGridData _gridData({
-    required BuildContext context,
-    required double horizontalLineInterval,
-  }) {
-    final brightness = Theme.of(context).brightness;
-    final lineColor =
-        brightness == Brightness.light ? Colors.black38 : Colors.white38;
-
-    return FlGridData(
-      drawHorizontalLine: true,
-      horizontalInterval: horizontalLineInterval,
-      getDrawingHorizontalLine: (_) => FlLine(strokeWidth: 1, color: lineColor),
-    );
-  }
-
-  FlTitlesData _titlesData({
-    required BuildContext context,
-    required double verticalTitleInterval,
-  }) {
-    final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
-    final textTheme = theme.textTheme;
-    final textStyle = textTheme.bodyText2!.copyWith(
-        fontSize: 10,
-        fontWeight: FontWeight.w500,
-        color: colorScheme.onSurface);
-
-    return FlTitlesData(
-      leftTitles: SideTitles(showTitles: false),
-      topTitles: SideTitles(showTitles: false),
-      rightTitles: SideTitles(
-        showTitles: true,
-        interval: verticalTitleInterval,
-        reservedSize: 32,
-        getTitles: (costAsDouble) =>
-            Amount.fromDouble(costAsDouble).toLocalString(compact: true),
-        getTextStyles: (_) => textStyle,
-      ),
-      bottomTitles: SideTitles(
-        showTitles: true,
-        interval: 1,
-        getTitles: (weekOfMonth) =>
-            WeekOfMonth.fromInt(weekOfMonth.toInt()).toOrdinalString(),
-        getTextStyles: (_) => textStyle,
-      ),
-    );
-  }
-
-  FlBorderData _borderData({required BuildContext context}) {
-    final brightness = Theme.of(context).brightness;
-    final lineColor =
-        brightness == Brightness.light ? Colors.black38 : Colors.white38;
-
-    return FlBorderData(
-      show: true,
-      border: Border(
-        bottom: BorderSide(color: lineColor),
-      ),
-    );
-  }
-
-  BarTouchData _barTouchData({required BuildContext context}) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-    final brightness = theme.brightness;
-
-    late final Color backgroundColor;
-    late final Color textColor;
-    if (brightness == Brightness.light) {
-      backgroundColor = Colors.grey[850]!;
-      textColor = Colors.white;
-    } else {
-      backgroundColor = Colors.grey.shade200;
-      textColor = Colors.black;
-    }
-
-    return BarTouchData(
-      touchTooltipData: BarTouchTooltipData(
-        tooltipBgColor: backgroundColor,
-        tooltipRoundedRadius: 4,
-        tooltipPadding: EdgeInsets.all(8),
-        getTooltipItem: (_, __, barRod, ___) {
-          return BarTooltipItem(
-            Amount.fromDouble(barRod.y).toLocalString(),
-            textTheme.subtitle2!.copyWith(color: textColor),
-          );
-        },
-        fitInsideHorizontally: false,
-        fitInsideVertically: false,
-      ),
-    );
+  String _magnitudeToLabel(double magnitude) {
+    return Amount.fromDouble(magnitude).toLocalString(compact: true);
   }
 
   double _ceilingByPlaceValue(double n) {
