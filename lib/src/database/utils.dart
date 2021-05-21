9ee2311a019a073f3246564193ed3b67bbec0c40
jaan-c <jaan-c@users.noreply.github.com>
2021-05-21 12:20:47 +0800

    Removed assertNotNull since Map.cast already exists.

--- lib/src/database/utils.dart
+++ lib/src/database/utils.dart
@@ -1,9 +1,3 @@
-extension AssertNonNullRowValues on Map<String, Object?> {
-  Map<String, Object> assertNotNullValues() {
-    return Map.fromEntries(entries.map((e) => MapEntry(e.key, e.value!)));
-  }
-}
-
 extension ListFisrtTry<T> on List<T> {
   T? get firstTry {
     try {
