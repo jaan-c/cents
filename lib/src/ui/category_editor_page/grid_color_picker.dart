de5492ac0a28be28d525ee178471efd852d60b7c
jaan-c <jaan-c@users.noreply.github.com>
2021-07-08 11:25:50 +0800

    Don't check if selectedColor is in colors if it's null.

---
+++ lib/src/ui/category_editor_page/grid_color_picker.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+typedef SelectColorCallback = void Function(Color);
+
+class GridColorPicker extends StatelessWidget {
+  final List<Color> colors;
+  final Color? selectedColor;
+  final SelectColorCallback onSelectColor;
+
+  GridColorPicker({
+    required this.colors,
+    required this.selectedColor,
+    required this.onSelectColor,
+  }) : assert(selectedColor == null || colors.contains(selectedColor));
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        for (final color in colors)
+          _colorTile(
+            context: context,
+            color: color,
+            isSelected: color == selectedColor,
+            onSelectColor: onSelectColor,
+          ),
+      ],
+    );
+  }
+
+  Widget _colorTile({
+    required BuildContext context,
+    required Color color,
+    required bool isSelected,
+    required SelectColorCallback onSelectColor,
+  }) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return InkWell(
+      onTap: () => onSelectColor(color),
+      child: Container(
+        width: kMinInteractiveDimension,
+        height: kMinInteractiveDimension,
+        color: color,
+        decoration: BoxDecoration(
+          border: Border.all(width: 1, color: colorScheme.onSurface),
+        ),
+        child: isSelected ? Icon(Icons.check_rounded) : null,
+      ),
+    );
+  }
+}
