bfc8ad9c607b0cecc962b7d92f3e6cdf961acb42
jaan-c <jaan-c@users.noreply.github.com>
2021-07-12 07:17:50 +0800

    Cache colors so it isn't constructed on every function call.

--- lib/src/ui/expense_editor_page/utils.dart
+++ lib/src/ui/expense_editor_page/utils.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-Color textToColor(Brightness brightness, String text) {
-  final colors = [
-    Colors.pink,
-    Colors.red,
-    Colors.deepOrange,
-    Colors.orange,
-    Colors.amber,
-    Colors.yellow,
-    Colors.lime,
-    Colors.lightGreen,
-    Colors.green,
-    Colors.teal,
-    Colors.cyan,
-    Colors.lightBlue,
-    Colors.blue,
-    Colors.indigo,
-    Colors.purple,
-    Colors.deepPurple,
-    Colors.blueGrey,
-    Colors.brown,
-  ].map((e) => e[300]!).toList();
+final _colors = [
+  Colors.pink,
+  Colors.red,
+  Colors.deepOrange,
+  Colors.orange,
+  Colors.amber,
+  Colors.yellow,
+  Colors.lime,
+  Colors.lightGreen,
+  Colors.green,
+  Colors.teal,
+  Colors.cyan,
+  Colors.lightBlue,
+  Colors.blue,
+  Colors.indigo,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.blueGrey,
+  Colors.brown,
+].map((e) => e[300]!).toList();
 
-  final ix = math.Random(text.hashCode).nextInt(colors.length);
-  return colors[ix];
+Color textToColor(Brightness brightness, String text) {
+  final ix = math.Random(text.hashCode).nextInt(_colors.length);
+  return _colors[ix];
 }
