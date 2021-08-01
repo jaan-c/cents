c3988225c6fcdd62cd49fdad2301f7a1dfc915bf
jaan-c <jaan-c@users.noreply.github.com>
2021-08-02 05:03:21 +0800

    Compute value property from partitionValue instead of asking for it in the constructor.
    It doesn't make any sense!

--- lib/src/ui/widgets/partitioned_bar_chart/partitioned_bar_data.dart
+++ lib/src/ui/widgets/partitioned_bar_chart/partitioned_bar_data.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 
 class PartitionedBarData {
-  final double value;
   final String label;
   final List<double> partitionValues;
   final List<Color> partitionColors;
   final String tooltipText;
 
+  double get value => partitionValues.fold(0.0, (acc, v) => acc + v);
+
   PartitionedBarData({
-    required this.value,
+    this.partitionValues = const [],
     required this.label,
-    List<double> partitionValues = const [],
-    List<Color> partitionColors = const [],
+    this.partitionColors = const [],
     this.tooltipText = '',
-  })  : assert(partitionValues.fold<double>(0.0, (a, b) => a + b) == value),
-        assert(partitionValues.length == partitionColors.length),
-        partitionValues =
-            partitionValues.isNotEmpty ? partitionValues : [value],
-        partitionColors =
-            partitionColors.isNotEmpty ? partitionColors : [Colors.blue];
+  }) : assert(partitionValues.length == partitionColors.length);
 }
