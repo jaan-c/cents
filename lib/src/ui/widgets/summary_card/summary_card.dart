222a9f9707483898a505ac9477d6aa01e2f22cab
jaan-c <jaan-c@users.noreply.github.com>
2021-08-06 12:13:08 +0800

    Rewrite SummaryCard parameters so it doesn't depend on vague inference.

--- lib/src/ui/widgets/summary_card/summary_card.dart
+++ lib/src/ui/widgets/summary_card/summary_card.dart
@@ -5,35 +5,65 @@ import 'package:flutter/material.dart' hide DateTimeRange;
 import 'month_summary_content.dart';
 import 'week_summary_content.dart';
 
-typedef SetDateTimeRangeCallback = void Function(DateTimeRange);
-
 class SummaryCard extends StatelessWidget {
-  final DateTimeRange dateTimeRange;
-  final List<Expense> expenses;
-  final SetDateTimeRangeCallback onSetDateTimeSpan;
+  final SummaryCardData data;
 
-  const SummaryCard({
-    required this.dateTimeRange,
-    required this.expenses,
-    required this.onSetDateTimeSpan,
-  }) : assert(dateTimeRange is WeekRange || dateTimeRange is MonthRange);
+  const SummaryCard({required this.data});
 
   @override
   Widget build(BuildContext context) {
-    if (dateTimeRange is WeekRange) {
+    if (data is WeekSummaryCardData) {
       return WeekSummaryContent(
-        weekRange: dateTimeRange as WeekRange,
-        expenses: expenses,
-        onSetWeekRange: onSetDateTimeSpan,
+        weekRange: data.dateTimeRange as WeekRange,
+        expenses: data.expenses,
+        onSetWeekRange: data.onSetDateTimeRange,
       );
-    } else if (dateTimeRange is MonthRange) {
+    } else if (data is MonthSummaryCardData) {
       return MonthSummaryContent(
-        monthRange: dateTimeRange as MonthRange,
-        expenses: expenses,
-        onSetMonthRange: onSetDateTimeSpan,
+        monthRange: data.dateTimeRange as MonthRange,
+        expenses: data.expenses,
+        onSetMonthRange: data.onSetDateTimeRange,
       );
     } else {
       throw StateError('dateTimeRange must be $WeekRange or $MonthRange');
     }
   }
+}
+
+typedef SetDateTimeRangeCallback<T extends DateTimeRange> = void Function(T);
+
+abstract class SummaryCardData<T extends DateTimeRange> {
+  abstract final T dateTimeRange;
+  abstract final List<Expense> expenses;
+  abstract final SetDateTimeRangeCallback<T> onSetDateTimeRange;
+}
+
+class WeekSummaryCardData implements SummaryCardData<WeekRange> {
+  @override
+  final WeekRange dateTimeRange;
+  @override
+  final List<Expense> expenses;
+  @override
+  final SetDateTimeRangeCallback<WeekRange> onSetDateTimeRange;
+
+  WeekSummaryCardData({
+    required this.dateTimeRange,
+    required this.expenses,
+    required this.onSetDateTimeRange,
+  });
+}
+
+class MonthSummaryCardData implements SummaryCardData<MonthRange> {
+  @override
+  final MonthRange dateTimeRange;
+  @override
+  final List<Expense> expenses;
+  @override
+  final SetDateTimeRangeCallback<MonthRange> onSetDateTimeRange;
+
+  MonthSummaryCardData({
+    required this.dateTimeRange,
+    required this.expenses,
+    required this.onSetDateTimeRange,
+  });
 }
